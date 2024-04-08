7080c6e23dd7009239e1e8de76fb81d3ba4d9f4e
Madeline Couse <mhcouse@gmail.com>
2024-04-08 13:38:04 -0400

    cleanup repo

--- crg2-pacbio.sh
+++ crg2-pacbio.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=crg2
-#SBATCH --time=72:00:00
+#SBATCH --job-name=crg2-pacbio
+#SBATCH --time=50:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=4G
 #SBATCH --output=%x-%j.out
@@ -8,7 +8,7 @@
 SF=~/crg2-pacbio/Snakefile
 CP="/hpf/largeprojects/ccm_dccforge/dccdipg/Common/snakemake"
 SLURM=~/crg2-pacbio/slurm_profile/
-CONFIG=config.yaml
+CONFIG="config.yaml"
 
 source /hpf/largeprojects/ccm_dccforge/dccdipg/Common/anaconda3/etc/profile.d/conda.sh
 conda activate snakemake
