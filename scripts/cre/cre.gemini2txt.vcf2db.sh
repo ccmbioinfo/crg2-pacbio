f2823f842f533166f0e959fe6a167c3807d22601
Madeline Couse <mhcouse@gmail.com>
2024-05-29 13:27:59 -0400

    move PS column and remove C4R_WES count columns

--- scripts/cre/cre.gemini2txt.vcf2db.sh
+++ scripts/cre/cre.gemini2txt.vcf2db.sh
@@ -97,7 +97,7 @@ sQuery="select \
         COALESCE(spliceai_score, '') as SpliceAI_score, \
         uce_100bp as UCE_100bp, uce_200bp as UCE_200bp, \
         Dark_genes as Dark_genes, \
-        ps as ps, \
+        ps as PS, \
         $noncoding_anno, \
         gts,"
 
