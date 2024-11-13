eb8db9c491e40968be8db2fae248411ae1311ee2
Madelinehazel <mhcouse@gmail.com>
2024-11-13 10:40:03 -0500

    add panel reports to minio

--- utils/copy_reports_to_minio.sh
+++ utils/copy_reports_to_minio.sh
@@ -15,13 +15,28 @@ $mc mb minio/results-c4r/long_read_seq/reports_2024/${family}
 # SNVs and indels
 $mc cp ${family}/*/small_variants/coding/${family}/*wes* minio/results-c4r/long_read_seq/reports_2024/${family}
 
+# panel report
+panel=${family}/*/small_variants/panel/*/*wgs*
+panel_date=`basename $panel | cut -d'.' -f3`
+$mc cp $panel minio/results-c4r/long_read_seq/reports_2024/${family}/${family}.wgs.panel.${panel_date}.csv
+
+# panel flank report
+panelflank=${family}/*/small_variants/panel-flank/*/*wgs*
+panel_date=`basename $panelflank | cut -d'.' -f3`
+$mc cp $panelflank minio/results-c4r/long_read_seq/reports_2024/${family}/${family}.wgs.panel-flank.${panel_date}.csv
+
 # SVs
 $mc cp ${family}/*/sv/${family}.pbsv.2*.csv minio/results-c4r/long_read_seq/reports_2024/${family}
 
 # pathogenic REs
 $mc cp ${family}/*/pathogenic_repeats/${family}.known.path.str.loci.2*.csv minio/results-c4r/long_read_seq/reports_2024/${family}
 
-# TCAG CNVs
-$mc cp ${family}/*/cnv/${family}*cnv.csv  minio/results-c4r/long_read_seq/reports_2024/${family}
+# repeat expansion outliers
+$mc cp ${family}/*/repeat_outliers/${family}.repeat.outliers.annotated.2*.csv minio/results-c4r/long_read_seq/reports_2024/${family}
 
-# TO DO: genome-wide repeats
+# TRGT de novo tandem repeats
+$mc cp ${family}/*/TRGT_denovo/${family}.TRGT.denovo.annotated.2*.csv minio/results-c4r/long_read_seq/reports_2024/${family}
+
+# TCAG CNVs
+$mc cp ${family}/*/cnv/${family}*cnv*csv  minio/results-c4r/long_read_seq/reports_2024/${family}
+
