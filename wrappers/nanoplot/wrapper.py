0312de3c9d040774bc552ccd456a33fd0ee0f0d3
Kobe Huynh <kkobehuynh@gmail.com>
2025-02-06 11:16:42 -0500

    Remove prefix argument from Nanoplot shell command in wrapper

--- wrappers/nanoplot/wrapper.py
+++ wrappers/nanoplot/wrapper.py
@@ -1,8 +1,7 @@
 from snakemake.shell import shell
 
 out_dir = snakemake.params.get("out_dir")
-prefix = snakemake.params.get("prefix")
 
 shell:
 
-"NanoPlot -t 4 --bam {snakemake.input} --prefix {prefix} --N50 --no_static --plots hex --outdir {out_dir}
+"NanoPlot -t 4 --bam {snakemake.input} --N50 --no_static --plots hex --outdir {out_dir}
