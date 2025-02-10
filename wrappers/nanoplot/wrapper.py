30c17a622d80c12dc34143d0c86a69dcf15fe22b
Kobe Huynh <kkobehuynh@gmail.com>
2025-02-10 14:23:04 -0500

    Fixed syntax

--- wrappers/nanoplot/wrapper.py
+++ wrappers/nanoplot/wrapper.py
@@ -1,7 +1,5 @@
 from snakemake.shell import shell
 
-out_dir = snakemake.params.get("out_dir")
+out_dir = snakemake.params.get("out_dir") 
 
-shell:
-
-"NanoPlot -t 4 --bam {snakemake.input} --N50 --no_static --plots hex --outdir {out_dir}
+shell("NanoPlot -t 4 --bam {snakemake.input} --N50 --no_static --plots hex --outdir {out_dir}")
