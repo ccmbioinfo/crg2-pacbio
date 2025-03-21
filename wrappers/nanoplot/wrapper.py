e4df0b6d41ce6771803a988f829028929c87eae6
Kobe Huynh <kkobehuynh@gmail.com>
2025-03-21 10:21:50 -0400

    Added Multiqc and Edited NanoPlot in wrapper and Snakefile

--- wrappers/nanoplot/wrapper.py
+++ wrappers/nanoplot/wrapper.py
@@ -2,4 +2,7 @@ from snakemake.shell import shell
 
 out_dir = snakemake.params.get("out_dir") 
 
-shell("NanoPlot -t 4 --bam {snakemake.input} --N50 --no_static --plots hex --outdir {out_dir}")
+shell(
+    "NanoPlot -t 4 --bam {snakemake.input} --N50 --plots hex --outdir {out_dir}"
+    "&& mv {out_dir}/Non_weightedHistogramReadlength.png {out_dir}/Non_Weighted_Histogram_Read_Length_mqc.png"
+    )
