c844a9faea1e7347bbf36ef0e940ca72fe9995e5
Rohan Khan <rohank.edu@gmail.com>
2026-06-15 16:58:58 -0400

    Replace blanks with .

--- scripts/mt_report.py
+++ scripts/mt_report.py
@@ -30,7 +30,7 @@ def remove_cols(df):
         "TIER",
         "REF_DEPTH",
         "TOTAL_LOCUS_DEPTH",
-        "VARIANT QUALITY",
+        "VARIANT_QUALITY",
         "QUAL",
         "MQM_INFO",
         "MQMR_INFO",
@@ -462,7 +462,7 @@ def main(vcf, report, family):
     final_report = check_sort(vcf_df,final_report)
     final_report = reorder_cols(final_report)
     final_report.columns = [col.replace(" ", "_") for col in final_report.columns]
-    final_report.columns = [col.replace(" ", "_") for col in final_report.columns]
+    final_report = final_report.fillna(".").replace(r"^\s*$", ".", regex=True)
 
     today = date.today()
     today = today.strftime("%Y-%m-%d")
