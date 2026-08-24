25ed04d9e32649eb58eb781dab10d6a5025093cc
Rohan Khan <rohank.edu@gmail.com>
2026-08-24 11:21:37 -0400

    Remove snpEff annotations from input VCFs

--- wrappers/input_prep/wrapper.py
+++ wrappers/input_prep/wrapper.py
@@ -7,6 +7,17 @@ units = pd.read_table(snakemake.input.units, dtype=str).set_index(["family"], dr
 input_vcf=units.loc[family, "small_variant_vcf"]
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+# For ONT VCFs, they maybe already be annotated with snpEff, which causes problems downstream
+# Remove the snpEff annotations.
 shell(
-    "( bcftools filter -e 'STRLEN(REF) >=50 | STRLEN(ALT) >=50' {input_vcf} -O z -o filtered/{family}.vcf.gz ) {log}"
+    """
+    (
+        if bcftools view -h {input_vcf} | grep '^##INFO=<ID=ANN,' > /dev/null; then
+            bcftools annotate -x INFO/ANN,INFO/LOF,INFO/NMD {input_vcf} -Ou \
+            | bcftools filter -e 'STRLEN(REF) >=50 | STRLEN(ALT) >=50' -O z -o filtered/{family}.vcf.gz
+        else
+            bcftools filter -e 'STRLEN(REF) >=50 | STRLEN(ALT) >=50' {input_vcf} -O z -o filtered/{family}.vcf.gz
+        fi
+    ) {log}
+    """
 )
